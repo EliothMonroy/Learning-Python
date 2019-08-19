4c249e56ceaf8314d0ff49259a90c8404561f93e
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2019-08-18 20:01:24 -0500

    [Added] Folder for Python Course 2017

---
+++ Curso-Python-2017/first_example.py
@@ -0,0 +1,11 @@
+import turtle
+window=turtle.Screen()
+dave=turtle.Turtle()
+dave.forward(50)
+dave.left(90)
+dave.forward(50)
+dave.left(90)
+dave.forward(50)
+dave.left(90)
+dave.forward(50)
+dave.left(90)
